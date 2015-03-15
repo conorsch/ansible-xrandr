72aed3b2fd91f45eeb35a0b731a31fdd6cbdb474
Conor Schaefer <conor.schaefer@gmail.com>
2015-03-15 16:14:41 -0400

    v0.1
    Squashed commit of the following:
    
    commit fa5fdd98ca23e436ec874a380778020b6d1becf4
    Author: Conor Schaefer <conor.schaefer@gmail.com>
    Date:   Sun Mar 15 16:13:17 2015 -0400
    
        adds some docs
    
        Squashed commit of the following:
    
        commit 07a169280ba64e8dc10c03608c7d26c8027fba84
        Author: Conor Schaefer <conor.schaefer@gmail.com>
        Date:   Sun Mar 15 16:13:04 2015 -0400
    
            adds README
    
        commit 01931f528ba32640777c56ecd7b9aca7080823b9
        Author: Conor Schaefer <conor.schaefer@gmail.com>
        Date:   Sun Mar 15 16:05:01 2015 -0400
    
            uses exit_json
    
        commit f4513bbd347883783691d7fc95b78dfa5e6d38e0
        Author: Conor Schaefer <conor.schaefer@gmail.com>
        Date:   Sun Mar 15 15:44:07 2015 -0400
    
            adds docstring to main
    
        commit 8346b462537fb1215f2f63d7ef7fb61c4dd8af50
        Author: Conor Schaefer <conor.schaefer@gmail.com>
        Date:   Sun Mar 15 15:43:00 2015 -0400
    
            cleans up imports
    
        commit 724e020981ff7ed73b6592ebbef9028771586619
        Author: Conor Schaefer <conor.schaefer@gmail.com>
        Date:   Sun Mar 15 15:42:29 2015 -0400
    
            adds documentation
    
    commit d001bdb3e1880fa118fc565b0efa8b2c0b7fd4d7
    Author: Conor Schaefer <conor.schaefer@gmail.com>
    Date:   Thu Mar 12 23:21:06 2015 -0400
    
        better regex
    
        Squashed commit of the following:
    
        commit 4b904b21251abd1086ec20143c9ea27f1a6d7954
        Author: Conor Schaefer <conor.schaefer@gmail.com>
        Date:   Thu Mar 12 23:20:53 2015 -0400
    
            moves string resolution to regex
    
        commit 9879315f1f41696b968c916a109b84850ea1c0e3
        Author: Conor Schaefer <conor.schaefer@gmail.com>
        Date:   Thu Mar 12 23:19:51 2015 -0400
    
            improves regex
    
        commit 59dcdadc28309792df976bb78b08282b8aef89c1
        Author: Conor Schaefer <conor.schaefer@gmail.com>
        Date:   Thu Mar 12 23:15:26 2015 -0400
    
            search for xrandr output line matching regex
    
        commit 1ec7135f8c7cc5a9fb3f416499dc7e233a381a74
        Author: Conor Schaefer <conor.schaefer@gmail.com>
        Date:   Thu Mar 12 23:11:17 2015 -0400
    
            precompile regex
    
    commit 51922077682ca08015e88224803ae7b610727edc
    Author: Conor Schaefer <conor.schaefer@gmail.com>
    Date:   Thu Mar 12 23:07:50 2015 -0400
    
        adds ansible boilerplate
    
        Squashed commit of the following:
    
        commit 19d92b60a6334a1d9aa290bdcb00e7d548e43d92
        Author: Conor Schaefer <conor.schaefer@gmail.com>
        Date:   Thu Mar 12 23:07:41 2015 -0400
    
            fixes xrandr call
    
        commit 60f1770cad7d1062fdd757d4128196e65804dec2
        Author: Conor Schaefer <conor.schaefer@gmail.com>
        Date:   Thu Mar 12 23:04:32 2015 -0400
    
            adds fail_json
    
        commit 30765435200e48d6ebe77891c9a7eed3b376874b
        Author: Conor Schaefer <conor.schaefer@gmail.com>
        Date:   Thu Mar 12 23:01:57 2015 -0400
    
            adds ansible boilerplate

---
+++ README.md
@@ -0,0 +1,13 @@
+ansible-xrandr
+==============
+Ansible module for querying display resolution info.
+
+Requirements
+============
+ * xrandr must be installed on target machine
+ * graphical environment must exist on target machine
+
+
+License
+=======
+MIT
--- xrandr.py
+++ xrandr.py
@@ -1,30 +1,43 @@
 #!/usr/bin/python
+DOCUMENTATION = '''
+---
+module: xrandr
+short_description: Wrapper for xrandr to retrieve display screen resolution settings.
+author: Conor Schaefer
+requirements:
+  - xrandr system package installed on target host
+'''
 
-import datetime
-import sys
 import json
-import os
-import shlex
 import subprocess
 import re
 
 
-
 def main():
+    """Call xrandr and return screen resolution."""
+
+    module = AnsibleModule(
+        argument_spec = dict(
+            something = dict(aliases=['whatever'])
+        )
+    )
 
     try:
-        display_info = subprocess.check_output('xrandsadr')
+        display_info = subprocess.check_output('xrandr').split("\n")
     except OSError:
-        print("xrandr is not installed.")
-        sys.exit(1)
+        module.fail_json(msg="xrandr is not installed.")
+
+    # example output:
+    # eDP1 connected 2560x1440+0+0 (normal left inverted right x axis y axis) 310mm x 174mm
+    regex = re.compile(r'^(?P<display_name>\w+) connected (?P<resolution_as_string>(?P<horizontal_resolution>\d+)x(?P<vertical_resolution>\d+)).*')
+    try:
+        r = filter(regex.match, display_info)[0]
+    except IndexError:
+        module.fail_json(msg="no connected screens found.")
+
+    matches = regex.match(r).groupdict()
+    module.exit_json(changed=False, result=matches)
 
 
-    r = display_info.split("\n")[0]
-    regex = r'.*current (?P<horizontal_resolution>\d+) x (?P<vertical_resolution>\d+),'
-
-    matches = re.match(regex, r).groupdict()
-    results = dict(changed=False, matches**)
-    print(results)
-
-
-
+from ansible.module_utils.basic import *
+main()
